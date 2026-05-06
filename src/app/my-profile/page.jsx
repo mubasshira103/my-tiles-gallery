c9efbe7c54ee1cb2fd421714599921684438e6ab
mubasshira103 <khadijamubasshira103@gmail.com>
2026-05-06 16:26:43 +0600

    updated my profilr

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+"use client";
+
+
+import { UpdateUserModal } from "@/component/UpdateUserModel";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Card } from "@heroui/react";
+
 
 const ProfilePage = () => {
-    return (
-        <div>
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
 
-        </div>
-    );
+
+  return (
+    <div>
+      <Card className="max-w-96 mx-auto flex flex-col items-center border mt-5">
+        <Avatar className="h-20 w-20">
+          <Avatar.Image
+            alt="John Doe"
+            src={user?.image}
+            referrerPolicy="no-referrer"
+          />
+          <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+        </Avatar>
+
+        <h2 className="text-xl text-gray-900 font-bold">{user?.name}</h2>
+        <p className="text-muted">{user?.email}</p>
+
+        <UpdateUserModal/>
+      </Card>
+    </div>
+  );
 };
 
 export default ProfilePage;
